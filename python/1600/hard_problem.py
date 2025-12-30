80fdc9bbcea422ebc3f296f812f877cb488285dd
firedespire <7sebastianbo11@gmail.com>
2025-12-30 18:02:35 -0500

    Add solution to Hard Problem (1600 rating)

--- python/1600/hard_problem.py
+++ python/1600/hard_problem.py
@@ -1,3 +1,10 @@
+# problem : https://codeforces.com/problemset/problem/706/C
+# At every index in the list of strings you can choose to either reverse them or not reverse them
+# Choosing to reverse costs energy and choosing not to doesn't cost anything. However, we must keep the array valid
+# So, if our choice leads to an invalid array we must set the value at a maximum (so that when choosing the best this will be removed as soon as possible)
+# If all values are the max value, there is no possible configuration which means we must output -1
+# At each index we choose the best answer: minimum (we must check if every possibility is valid in order to remove the invalid ones)
+
 n = int(input())
 
 nums = list(map(int, input().split(" ")))
