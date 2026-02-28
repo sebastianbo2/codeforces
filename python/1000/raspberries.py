d292b6e043b6a1136ebb7d7d7ccd4fdb9d94a603
firedespire <7sebastianbo11@gmail.com>
2026-02-28 11:00:30 -0500

    Add solution for Raspberries (1000)

--- python/1000/raspberries.py
+++ python/1000/raspberries.py
@@ -1,3 +1,10 @@
+# problem : https://codeforces.com/problemset/problem/1883/C
+
+# all possible k values exept for 4 are primes so we just note the small different from it and thats out answer
+# for 4, we either need 0 operations (one number is divisible by 4 so the product is too), 1 operation (one number is 1 away from being divisible by 4),
+# or 2 (2 odd numbers increase by 1 making them even, so the product divides 2 twice meaning that it divides 4)
+# my solution is unfortunately far from being the best for this problem since it has alot of ifs, however its what i thought of initially and it works so its not that bad
+
 import math
 
 for t in range(int(input())):
